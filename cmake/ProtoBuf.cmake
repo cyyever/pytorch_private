cb9d89d523b68f6c73791b5ad9672d1052598928
cyy <cyyever@outlook.com>
2021-06-20 17:52:42 +0800

    use pkgconfig to detect nccl
    remove cmake target check as we always use new packages
    
    simpify openmp detection
    
    simpify protobuf detection
    
    simpify threads detection
    
    use private libs
    
    fix install
    
    use public dep
    
    change openblas detection
    
    fix eigen
    
    fix pybind11
    
    disable check_submodules
    
    fix openblas
    
    simplify code
    
    add CUDA to lang
    
    use c++17
    
    simplify cuda code
    
    remove unused check
    
    fix syntax
    
    comment out code

--- cmake/ProtoBuf.cmake
+++ cmake/ProtoBuf.cmake
@@ -1,125 +1,15 @@
 # Finds Google Protocol Buffers library and compilers and extends
 # the standard cmake script with version and python generation support
-macro(custom_protobuf_find)
-  message(STATUS "Use custom protobuf build.")
-  option(protobuf_BUILD_TESTS "" OFF)
-  option(protobuf_BUILD_EXAMPLES "" OFF)
-  option(protobuf_WITH_ZLIB "" OFF)
-  if(APPLE)
-    # Protobuf generated files triggers a deprecated atomic operation warning
-    # so we turn it off here.
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations")
-  endif()
-  if(${CAFFE2_LINK_LOCAL_PROTOBUF})
-    # If we are going to link protobuf locally, we will need to turn off
-    # shared libs build for protobuf.
-    option(protobuf_BUILD_SHARED_LIBS "" OFF)
-  else()
-    # If we are building Caffe2 as shared libs, we will also build protobuf as
-    # shared libs.
-    option(protobuf_BUILD_SHARED_LIBS "" ${BUILD_SHARED_LIBS})
-  endif()
-  # We will make sure that protobuf and caffe2 uses the same msvc runtime.
-  option(protobuf_MSVC_STATIC_RUNTIME "" ${CAFFE2_USE_MSVC_STATIC_RUNTIME})
-
-  if(${CAFFE2_LINK_LOCAL_PROTOBUF})
-    set(__caffe2_CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ${CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS})
-    set(__caffe2_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS OFF)
-    set(BUILD_SHARED_LIBS OFF)
-    if(${COMPILER_SUPPORTS_HIDDEN_VISIBILITY})
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden")
-    endif()
-    if(${COMPILER_SUPPORTS_HIDDEN_INLINE_VISIBILITY})
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility-inlines-hidden")
-    endif()
-  endif()
-
-  set(__caffe2_CMAKE_POSITION_INDEPENDENT_CODE ${CMAKE_POSITION_INDEPENDENT_CODE})
-  set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-
-  if(MSVC)
-    foreach(flag_var
-        CMAKE_C_FLAGS CMAKE_C_FLAGS_RELEASE CMAKE_C_FLAGS_MINSIZEREL
-        CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL)
-      if(${flag_var} MATCHES "/Z[iI7]")
-        string(REGEX REPLACE "/Z[iI7]" "" ${flag_var} "${${flag_var}}")
-      endif()
-    endforeach(flag_var)
-    if(MSVC_Z7_OVERRIDE)
-      foreach(flag_var
-          CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELWITHDEBINFO
-          CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-        if(${flag_var} MATCHES "/Z[iI]")
-          string(REGEX REPLACE "/Z[iI]" "/Z7" ${flag_var} "${${flag_var}}")
-        endif()
-      endforeach(flag_var)
-    endif(MSVC_Z7_OVERRIDE)
-  endif(MSVC)
-
-  add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/protobuf/cmake)
-
-  set(CMAKE_POSITION_INDEPENDENT_CODE ${__caffe2_CMAKE_POSITION_INDEPENDENT_CODE})
-
-  if(${CAFFE2_LINK_LOCAL_PROTOBUF})
-    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ${__caffe2_CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS})
-    set(BUILD_SHARED_LIBS ON)
-    set(CMAKE_CXX_FLAGS ${__caffe2_CMAKE_CXX_FLAGS})
-  endif()
-
-  # Protobuf "namespaced" target is only added post protobuf 3.5.1. As a
-  # result, for older versions, we will manually add alias.
-  if(NOT TARGET protobuf::libprotobuf)
-    add_library(protobuf::libprotobuf ALIAS libprotobuf)
-    add_library(protobuf::libprotobuf-lite ALIAS libprotobuf-lite)
-    # There is link error when cross compiling protoc on mobile:
-    # https://github.com/protocolbuffers/protobuf/issues/2719
-    # And protoc is very unlikely needed for mobile builds.
-    if(NOT (ANDROID OR IOS))
-      add_executable(protobuf::protoc ALIAS protoc)
-    endif()
-  endif()
-endmacro()
 
 # Main entry for protobuf. If we are building on Android, iOS or we have hard
 # coded BUILD_CUSTOM_PROTOBUF, we will hard code the use of custom protobuf
 # in the submodule.
-if(ANDROID OR IOS)
-  if(NOT BUILD_CUSTOM_PROTOBUF)
-    message(WARNING
-        "For Android and iOS cross compilation, I am automatically using "
-        "custom protobuf under third party. Note that this behavior may "
-        "change in the future, and you will need to specify "
-        "-DBUILD_CUSTOM_PROTOBUF=ON explicitly.")
-  endif()
-  # There is link error when cross compiling protoc on mobile:
-  # https://github.com/protocolbuffers/protobuf/issues/2719
-  # And protoc is very unlikely needed for mobile builds.
-  set(__caffe2_protobuf_BUILD_PROTOC_BINARIES ${protobuf_BUILD_PROTOC_BINARIES})
-  set(protobuf_BUILD_PROTOC_BINARIES OFF CACHE BOOL "" FORCE)
-  custom_protobuf_find()
-  set(protobuf_BUILD_PROTOC_BINARIES ${__caffe2_protobuf_BUILD_PROTOC_BINARIES} CACHE BOOL "" FORCE)
-elseif(BUILD_CUSTOM_PROTOBUF)
-  message(STATUS "Building using own protobuf under third_party per request.")
-  custom_protobuf_find()
-else()
-  include(cmake/public/protobuf.cmake)
-endif()
 
+find_package(protobuf)
 if((NOT TARGET protobuf::libprotobuf) AND (NOT TARGET protobuf::libprotobuf-lite))
-  message(WARNING
-      "Protobuf cannot be found. Caffe2 will automatically switch to use "
-      "own protobuf under third_party. Note that this behavior may change in "
-      "the future, and you will need to specify -DBUILD_CUSTOM_PROTOBUF=ON "
-      "explicitly.")
-  custom_protobuf_find()
-
-  # TODO(jiayq): enable this in the future, when Jenkins Mac support is
-  # properly set up with protobuf installs.
-
-  # message(FATAL_ERROR
-  #     "Protobuf cannot be found. Caffe2 will have to build with libprotobuf. "
-  #     "Please set the proper paths so that I can find protobuf correctly.")
+  message(FATAL_ERROR
+      "Protobuf cannot be found. Caffe2 will have to build with libprotobuf. "
+      "Please set the proper paths so that I can find protobuf correctly.")
 endif()
 
 get_target_property(__tmp protobuf::libprotobuf INTERFACE_INCLUDE_DIRECTORIES)
