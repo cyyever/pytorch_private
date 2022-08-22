f29a7fc16d74e3d51c326278043309c586330e05
cyy <cyyever@outlook.com>
2022-08-22 11:40:36 +0800

    cleanup cuda cmake files

--- cmake/public/cuda.cmake
+++ cmake/public/cuda.cmake
@@ -1,7 +1,7 @@
 # ---[ cuda
 include_guard(GLOBAL)
 
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../Modules_CUDA_fix)
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
 # Find CUDA.
 find_package(CUDAToolkit)
