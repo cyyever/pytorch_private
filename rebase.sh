db8c49ed60e574e3a9decd6dbd2595eddf8f288e
cyy <cyyever@outlook.com>
2023-04-17 15:09:33 +0800

    add rebase script

---
+++ rebase.sh
@@ -0,0 +1,22 @@
+set -eu
+git branch -D cyy || true
+git push -d origin cyy || true
+
+# branches="build_system_fix miscellaneous_fixes ubsan fix_performance msvc_ssize use_dynamic_cast cxx20 detailed_error fixwarning various_fix cpp17 fix_gil_bug"
+# branches="build_system_fix use_dynamic_cast detailed_error fix_gil_bug"
+branches="fix_gil_bug"
+for branch in $branches; do
+  echo "rebase ${branch}"
+  git branch -D ${branch} || true
+  git checkout ${branch}
+  # git rebase up/master
+  # git push --force
+  echo "end rebase ${branch}"
+done
+git checkout -b cyy
+for branch in $branches; do
+  echo "rebase ${branch}"
+  git rebase ${branch}
+  echo "end rebase ${branch}"
+done
+git push --set-upstream origin cyy
